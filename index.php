a9fe5e2379e4e0427062bc32b88a7a498c74389f
fcoverdugoa <fcoverdugoa@underpost.net>
2021-02-22 20:13:51 -0300

    fix

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta name='viewport' content='initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
   <meta name='viewport' content='width=device-width, user-scalable=no' />
   <!-- <link rel='icon' href='/cloud/e_learning/silvio/favicon.ico' type='image/x-icon' /> -->
-  <link rel='icon' href='/app/favicon.ico' type='image/x-icon' />-->
+  <link rel='icon' href='/app/favicon.ico' type='image/x-icon' />
   <script>
   <?php
 
